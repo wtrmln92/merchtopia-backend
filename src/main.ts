d93c20622a274a3f3806f06eb4fe10a2e98fec51
Wai Kit Koh <kit@macbookpro.lan>
2025-12-14 14:52:54 +0800

    Add swagger api docs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,16 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:5173',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Merchtopia')
+    .setDescription('The Merchtopia API description')
+    .setVersion('1.0')
+    .addTag('Merchtopia')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,14 +1,14 @@
-import { IsNotEmpty, IsString} from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { Transform } from 'class-transformer';
 
 export class CreateProductDto {
   @IsString()
   @IsNotEmpty()
-  @Transform(({value}) => value.trim())
+  @Transform(({ value }) => value.trim())
   sku: string;
 
   @IsString()
   @IsNotEmpty()
-  @Transform(({value}) => value.trim())
+  @Transform(({ value }) => value.trim())
   displayName: string;
 }
